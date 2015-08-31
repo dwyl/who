76a9b274d74120b14d4045d96be604979376a8d3
Nelson <contact.nelsonic@gmail.com>
2015-08-31 07:10:30 +0100

    adds initial profile .json files for list of people who have made their member of dwyl public

--- people/_scraper.js
+++ people/_scraper.js
@@ -4,7 +4,7 @@ var fs  = require('fs');
 
 gs(url, function(err, data) {
   console.log(data.entries); // or what ever you want to do with the data
-  fs.writeFileSync(__dirname+'/members.txt', data.entries);
+  fs.writeFileSync(__dirname+'/_members.txt', data.entries);
   data.entries.forEach(function(person){
     gs('/'+person, function(err, profile){
       profile.github = person;
@@ -16,7 +16,7 @@ gs(url, function(err, data) {
         avatar: profile.avatar
       }
       console.log(json);
-      // fs.writeFileSync(__dirname+'/'+person+'.json' +JSON.stringify(json));
+      fs.writeFileSync(__dirname+'/'+person+'.json', JSON.stringify(json));
     });
   });
 })
