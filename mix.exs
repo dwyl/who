69a8b91c2d23a02a868f0529974a8c884fd074b8
nelsonic <nelson+github@dwyl.com>
2022-10-05 06:37:44 +0100

    borrow deps and aliases from mvp/mix.exs

---
+++ mix.exs
@@ -0,0 +1,99 @@
+defmodule App.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :app,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        c: :test,
+        coveralls: :test,
+        "coveralls.json": :test,
+        "coveralls.html": :test,
+        t: :test
+      ]
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {App.Application, []},
+      extra_applications: [:logger, :runtime_tools]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "~> 1.6.12"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.17.5"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:esbuild, "~> 0.4", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 1.0"},
+      {:jason, "~> 1.2"},
+      {:plug_cowboy, "~> 2.5"},
+
+      # Create docs on localhost by running "mix docs"
+      {:ex_doc, "~> 0.28.4", only: :dev, runtime: false},
+      # Track test coverage
+      {:excoveralls, "~> 0.14.5", only: [:test, :dev]},
+      # git pre-commit hook runs tests before allowing commits
+      {:pre_commit, "~> 0.3.4"},
+      # Credo static analysis: https://github.com/rrrene/credo
+      {:credo, "~> 1.6.4", only: [:dev, :test], runtime: false},
+
+      # Ref: github.com/dwyl/learn-tailwind
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, to install project dependencies and perform other setup tasks, run:
+  #
+  #     $ mix setup
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    [
+      seeds: ["run priv/repo/seeds.exs"],
+      setup: ["deps.get", "ecto.reset", "tailwind.install"],
+      "ecto.setup": [
+        "ecto.create --quiet",
+        "ecto.migrate --quiet",
+        "run priv/repo/seeds.exs"
+      ],
+      "ecto.reset": ["ecto.drop --quiet", "ecto.setup"],
+      "assets.deploy": [
+        "tailwind default --minify",
+        "esbuild default --minify",
+        "phx.digest"
+      ],
+      test: ["ecto.reset", "test"],
+      t: ["test"],
+      c: ["coveralls.html"],
+      s: ["phx.server"]
+    ]
+  end
+end
