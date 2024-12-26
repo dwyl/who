4efa7eade247efd957e71ecbb408d9a0693d727c
nelsonic <nelson+github@dwyl.com>
2024-12-26 07:56:49 +0000

    App.GitHub.org_user_list/1 now returns *all* 521 users in a single API request #17

--- test/app/github_test.exs
+++ test/app/github_test.exs
@@ -14,4 +14,10 @@ defmodule App.GitHubTest do
     user = GitHub.user(username) # |> dbg
     assert user.public_repos > 30
   end
+
+  test "App.org_user_list/1" do
+    orgname = "dwyl"
+    list = GitHub.org_user_list(orgname)
+    assert length(list) > 500
+  end
 end
