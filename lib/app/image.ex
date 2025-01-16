925dee98fb02d7d119cf004363a794db2dec1ccc
nelsonic <nelson+github@dwyl.com>
2025-01-16 14:26:00 +0000

    Img.get_avatar_color(avatar_url) implemented for #221

--- lib/app/image.ex
+++ lib/app/image.ex
@@ -1,4 +1,4 @@
-defmodule App.Image do
+defmodule App.Img do
   @moduledoc """
   Handles extracting color data from images.
   Uses: https://github.com/elixir-image/image
@@ -6,6 +6,7 @@ defmodule App.Image do
   require Logger
   :inets.start()
   :ssl.start()
+  @hex "0123456789ABCDEF"
 
   @doc """
   Retrieve raw image data from a URL.
@@ -23,6 +24,27 @@ defmodule App.Image do
   https://hexdocs.pm/image/Image.html#dominant_color/2
   """
   def extract_color(img_data) do
-    "blue"
+    {:ok, [r, g, b]} =
+      Image.open!(img_data)
+      |> Image.dominant_color()
+
+    rgb_to_hex(r, g, b)
+  end
+
+  def get_avatar_color(avatar_url) do
+    get_raw_image_data(avatar_url) |> extract_color()
+  end
+
+  # functions borrowed from:
+  # https://github.com/nelsonic/colors/blob/master/colors.html#L96
+  def rgb_to_hex(r, g, b) do
+    "#{to_hex(r)}#{to_hex(g)}#{to_hex(b)}"
+  end
+
+  # No error/bounds checking as values come from `Image.dominant_color/1`
+  def to_hex(n) do
+    mod = Integer.mod(n, 16)
+    rounded = trunc(Float.ceil((n - mod) / 16))
+    "#{String.at(@hex, rounded)}#{String.at(@hex, mod)}"
   end
 end
