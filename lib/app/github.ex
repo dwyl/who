fd16b4917b55a277b993e1b4d7c250d67313ccde
nelsonic <nelson+github@dwyl.com>
2025-01-27 20:47:46 +0000

    add code for API request logging #226

--- lib/app/github.ex
+++ lib/app/github.ex
@@ -3,7 +3,7 @@ defmodule App.GitHub do
   Handles all interactions with the GitHub REST API
   via: github.com/edgurgel/tentacat Elixir GitHub Lib.
   """
-  require Logger
+  import App.Reqlog, only: [log: 2]
 
   @access_token Application.compile_env(:tentacat, :access_token)
   @client Tentacat.Client.new(%{access_token: @access_token})
@@ -13,7 +13,8 @@ defmodule App.GitHub do
   Returns the GitHub repository data.
   """
   def repository(owner, reponame) do
-    Logger.info "Fetching repository #{owner}/#{reponame}"
+    log("repository", "#{owner}/#{reponame}")
+    # Logger.info "Fetching repository #{owner}/#{reponame}"
     {_status, data, _res} =
       Tentacat.Repositories.repo_get(@client, owner, reponame)
     data
@@ -23,7 +24,8 @@ defmodule App.GitHub do
   Returns the list of GitHub repositories for an Org.
   """
   def org_repos(owner) do
-    Logger.info "Fetching list of repositories for #{owner}"
+    log("org_repos", owner)
+    # Logger.info "Fetching list of repositories for #{owner}"
     {_status, data, _res} =
       Tentacat.Repositories.list_orgs(@client, owner)
     data
@@ -33,7 +35,8 @@ defmodule App.GitHub do
   `user/1` Returns the GitHub user profile data.
   """
   def user(username) do
-    Logger.info "Fetching user #{username}"
+    # Logger.info "Fetching user #{username}"
+    log("user", username)
     {_status, data, _res} = Tentacat.Users.find(@client, username)
     data
   end
@@ -42,7 +45,8 @@ defmodule App.GitHub do
   `org_user_list/1` Returns the list of GitHub users for an org.
   """
   def org_user_list(orgname) do
-    Logger.info "Fetching org user list for #{orgname}"
+    # Logger.info "Fetching org user list for #{orgname}"
+    log("org_user_list", orgname)
     {_status, data, _res} =
         Tentacat.Organizations.Members.list(@client, orgname)
     data
@@ -54,7 +58,8 @@ defmodule App.GitHub do
   `repo` - name of the repo to check stargazers for.
   """
   def repo_stargazers(owner, repo) do
-    Logger.info "Fetching stargazers for #{owner}/#{repo}"
+    log("repo_stargazers", "#{owner}/#{repo}")
+    # Logger.info "Fetching stargazers for #{owner}/#{repo}"
     {_status, data, _res} =
       Tentacat.Users.Starring.stargazers(@client, owner, repo)
     data
