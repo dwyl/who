4199fe5163505de5caf3276470a776838a218e9b
nelsonic <nelson+github@dwyl.com>
2025-01-29 08:03:32 +0000

    add docs for req_count_last_hour/0 #226

--- lib/app/reqlog.ex
+++ lib/app/reqlog.ex
@@ -29,12 +29,18 @@ defmodule App.Reqlog do
   end
 
   # clean interface function that can be called from App.GitHub functions
-  # e.g: log("repository", "#{owner}/#{reponame}")
+  # e.g: log("repository", "#{owner}/#{repo}")
   def log(req, param) do
     Logger.info "Fetching #{req} #{param}"
     create(%{req: req, param: param})
   end
 
+  @doc """
+  `req_count_last_hour/0` returns the count (integer) of how many API Requests
+  were made in the last hour to help us stay under the `5k/h` limit.
+  Sample SQL if you need to test this independently:
+  SELECT COUNT(*) FROM reqlogs WHERE inserted_at > '2025-01-27 11:02:50'
+  """
   def req_count_last_hour() do
     # Using `DateTime.add/4` with a negative number to subtract. ;-)
     # via: https://elixirforum.com/t/create-time-with-one-hour-plus/3666/5
